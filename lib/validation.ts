da84cfb8668e83a00f6b597e49c7917a8475c5a2
verobelo <142001535+verobelo@users.noreply.github.com>
2025-10-14 13:21:39 +0200

    Change key

--- lib/validation.ts
+++ lib/validation.ts
@@ -2,7 +2,7 @@ import z from 'zod';
 
 export const waitlistFormSchema = z.object({
   email: z.email('Please enter a valid email address'),
-  acceptedPrivacy: z.boolean().refine((val) => val === true, {
+  consent: z.boolean().refine((val) => val === true, {
     message: 'You must accept the privacy policy',
   }),
 });
