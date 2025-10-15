80145def8d24ef66ad35f67b2bd0b7e295d4fb01
verobelo <142001535+verobelo@users.noreply.github.com>
2025-10-15 17:12:25 +0200

    Add language context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins, Inter } from "next/font/google";
 import "./globals.css";
+import { LanguageProvider } from "@/context/LanguageContext";
 
 const poppins = Poppins({
   weight: ['400', '600', '700'],
@@ -29,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${poppins.variable} ${inter.variable} antialiased`}
       >
-        {children}
+        <LanguageProvider>
+          {children}
+        </LanguageProvider>
       </body>
     </html>
   );
