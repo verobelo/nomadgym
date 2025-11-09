64fc5a5a0110302ade9bc3c306f19b431bf815fe
verobelo <142001535+verobelo@users.noreply.github.com>
2025-11-09 18:28:14 +0100

    Add test for a go back button

--- tests/pages/notFound.test.tsx
+++ tests/pages/notFound.test.tsx
@@ -1,6 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
-import '@testing-library/jest-dom/vitest';
+import { vi } from 'vitest';
 import NotFound from '@/app/not-found';
 import { LanguageProvider } from '@/context/LanguageContext';
 
@@ -13,5 +13,22 @@ describe('NotFound', () => {
     );
     const heading = screen.getByRole('heading', { level: 1 });
     expect(heading).toBeInTheDocument();
+  });
+
+  it('should call window.history.back when back button is clicked', () => {
+    render(
+      <LanguageProvider>
+        <NotFound />
+      </LanguageProvider>
+    );
+
+    const backButton = screen.getByRole('button', { name: /back/i });
+    expect(backButton).toBeInTheDocument();
+
+    const goBack = vi.spyOn(window.history, 'back');
+    fireEvent.click(backButton);
+    expect(goBack).toHaveBeenCalledOnce();
+
+    goBack.mockRestore();
   });
 });
